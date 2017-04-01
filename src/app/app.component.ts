40b5e02dba5499a117142d04751561b7496578ce
ricevind <ricevind@gmail.com>
2017-04-01 11:20:38 +0200

    Working hot module reloader (no css reload with state retention)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
+  count = 1;
+
+  add() {
+    this.count += 1;
+  }
 }

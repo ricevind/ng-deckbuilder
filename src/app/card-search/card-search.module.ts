f47aeea6009fb5d755bfd8bb9db8540d77e15bbd
ricevind <ricevind@gmail.com>
2017-04-16 12:22:42 +0200

    creating card-search.module and card-search-routing.module

---
+++ src/app/card-search/card-search.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CardSearchRoutingModule } from './card-search-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CardSearchRoutingModule
+  ],
+  declarations: []
+})
+export class CardSearchModule { }

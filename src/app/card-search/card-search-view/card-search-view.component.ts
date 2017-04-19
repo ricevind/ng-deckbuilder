40bbeb5f68744be0b90502bee3742b5b668c7f51
ricevind <ricevind@gmail.com>
2017-04-19 18:31:20 +0200

    card interface

--- src/app/card-search/card-search-view/card-search-view.component.ts
+++ src/app/card-search/card-search-view/card-search-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HsApiService } from '../hs-api.service';
+import { Observable } from 'rxjs/Observable';
+import { Card } from '../card';
 
 @Component({
   selector: 'app-card-search-view',
@@ -6,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-search-view.component.scss']
 })
 export class CardSearchViewComponent implements OnInit {
+  public card: Card;
 
-  constructor() { }
+  constructor(private hsApi: HsApiService) {
+    this.hsApi.getCardByName('Ysera').subscribe(r => {this.card = r; });
+
+  }
 
   ngOnInit() {
   }
---
+++ src/app/card-search/card.ts
@@ -0,0 +1,22 @@
+export interface Card {
+
+    'cardId': '';
+    'name': '';
+    'cardSet'?: '';
+    'type'?: '';
+    'faction'?: '';
+    'rarity'?: '';
+    'cost': number;
+    'attack': number;
+    'health': number;
+    'text'?: '';
+    'flavor'?: '';
+    'artist'?: '';
+    'collectible'?: boolean;
+    'elite'?: boolean;
+    'race'?: '';
+    'img': '';
+    'imgGold'?: '';
+    'locale'?: '';
+
+}

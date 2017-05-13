8e82788f30b544c0719bdbb82ce3bdf4c87a3648
ricevind <ricevind@gmail.com>
2017-05-13 10:30:13 +0200

    basic deck service

--- src/app/shared/deck.service.ts
+++ src/app/shared/deck.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Deck } from './models/deck';
+import { Observable } from 'rxjs/Observable';
+import { Card } from './models/card';
 
 @Injectable()
 export class DeckService {
+  private _deck: BehaviorSubject<Deck|null> = new BehaviorSubject(null);
+  private deck$: Observable<Deck|null> = this._deck.asObservable();
 
   constructor() { }
+
+  get deckStream(): Observable<Deck> {
+    return this.deck$;
+  }
+
+  getDeck(): Deck {
+    return this._deck.getValue();
+  }
+
+  setDeck(deck: Deck): void {
+    const newDeck = JSON.parse(JSON.stringify(deck));
+    this._deck.next(newDeck);
+  }
+
+  addCard(card: Card): void {
+    const currentDeck = this.getDeck();
+    currentDeck.cards.push(card);
+    this.setDeck(currentDeck);
+  }
+
+  removeCard(card: Card|string): void {
+    let cardName: string;
+    if (typeof card === 'object') {
+      cardName = card.name;
+    } else {
+      cardName = card;
+    }
+    const currentDeck = this.getDeck();
+    const updatedCards = currentDeck.cards.filter((cardInDeck: Card) => {
+      return cardInDeck.name !== cardName;
+    });
+    currentDeck.cards = updatedCards;
+    this.setDeck(currentDeck);
+  }
 
   // get deck
   // add card
--- src/app/shared/models/deck.ts
+++ src/app/shared/models/deck.ts
@@ -1,5 +1,6 @@
 import { Card } from './card';
 
 export interface Deck {
-    cards: Card[];
+    name: string;
+    cards?: Card[];
 }
